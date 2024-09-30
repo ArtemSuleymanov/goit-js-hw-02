ab47ef5ae4722b6323473bca470ac56833e2357c
Artem <suleymanov2007artem@gmail.com>
2024-09-30 20:56:50 +0300

    commit

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+     const totalPrice= quantity * pricePerDroid;
+
+     if (totalPrice > customerCredits) {
+         return "Insufficient funds!";
+     } else {
+        return "You ordered ${quantity} droids worth ${pricePerDroid} credits!";
+     }
+ }
+
+ console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+
